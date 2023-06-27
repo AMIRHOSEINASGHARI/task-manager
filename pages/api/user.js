070dac3f0c48d9d7938e795a1fec63be6d0ef5e5
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-06-27 12:09:15 +0330

    db

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,7 +1,17 @@
 import User from "@/models/user";
+import connectDB from "@/utils/connectDB";
 import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
+  //****/   connecting to data base function   /****//
+  try {
+    await connectDB();
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "failed", message: "Cannot connect to data base" });
+  }
   if (req.method === "GET") {
     const {
       user: { email },
